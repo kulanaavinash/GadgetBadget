5c37d1ae0be22692762cf450af2d8a7945060aef
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-04-19 14:01:18 +0530

    Create db connection

--- GadgetBadget/src/model/Customer.java
+++ GadgetBadget/src/model/Customer.java
@@ -1,5 +1,22 @@
 package model;
+import java.sql.*;
 
 public class Customer {
+	
+	public Connection connect() {
+		Connection con = null;
 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			 con= DriverManager.getConnection("jdbc:mysql://localhost:3306/gadgetbadget",
+			 "root", "");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
 }

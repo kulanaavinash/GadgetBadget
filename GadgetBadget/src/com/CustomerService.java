ff3e1f18b19d9956b58a78cc57593bece80356b2
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-04-19 22:43:49 +0530

    Retrive function

--- GadgetBadget/src/com/CustomerService.java
+++ GadgetBadget/src/com/CustomerService.java
@@ -1,5 +1,23 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Customer;
 
 
 
@@ -9,5 +27,15 @@ package com;
 @Path("/Customers")
 
 public class CustomerService {
+	
+	
+	Customer customerObj = new Customer();
+	 @GET
+	 @Path("/")
+	 @Produces(MediaType.TEXT_HTML)
+	 public String readCustomers()
+	  {
+	  return customerObj.readCustomers();
+	  }
 	
 }

801584906aeb3d72759b70e4545e45a187a0e382
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-04-20 20:19:10 +0530

    Get function

--- GadgetBadget/src/com/PaymentService.java
+++ GadgetBadget/src/com/PaymentService.java
@@ -1,5 +1,38 @@
 package com;
 
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Payment;
+
+@Path("/Payments")
+
 public class PaymentService {
+	
+	 Payment paymentObj = new Payment();
+	 @GET
+	 @Path("/")
+	 @Produces(MediaType.TEXT_HTML)
+	 public String readPayments()
+	  {
+	  return  paymentObj.readPayments();
+	  }
+	 
 	
 }

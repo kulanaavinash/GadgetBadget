80f7eae9e2f61a98793772d9491854d771b43062
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-04-20 18:33:08 +0530

    database connection

--- GadgetBadget/src/model/Payment.java
+++ GadgetBadget/src/model/Payment.java
@@ -1,5 +1,28 @@
 package model;
 
-public class Payment {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Payment {
+		
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			 con= DriverManager.getConnection("jdbc:mysql://localhost:3306/gadgetbadget",
+			 "root", "");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
 }

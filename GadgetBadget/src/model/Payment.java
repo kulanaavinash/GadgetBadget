c9dd6b22f6cdd0d85842ae7ada6c397dcb9a3aa7
Kulana Avinash <kulanaavinash2018@gmail.com>
2021-04-20 18:34:57 +0530

    insert method

--- GadgetBadget/src/model/Payment.java
+++ GadgetBadget/src/model/Payment.java
@@ -25,4 +25,38 @@ public class Payment {
 
 		return con;
 	}
+	
+	// insert method
+			public String insertPayment(String date, String number, String amount, String type) {
+				Connection con = connect();
+				String output = "";
+				if (con == null) {
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = " insert into payments (`PaymentID`,`PaymentDate`,`CardNumber`,`Amount`,`PaymentType`)"
+						+ " values (?, ?, ?, ?, ?)";
+				PreparedStatement preparedStmt;
+				try {
+					preparedStmt = con.prepareStatement(query);
+
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, date);
+					preparedStmt.setString(3, number);
+					preparedStmt.setString(4, amount);
+					preparedStmt.setString(5, type);
+
+					preparedStmt.execute();
+					con.close();
+					output = "Inserted successfully";
+				} catch (SQLException e) {
+					output = "Error while inserting";
+					System.err.println(e.getMessage());
+				}
+				// binding values
+
+				return output;
+			}
+	
 }

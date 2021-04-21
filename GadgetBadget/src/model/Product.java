b0af14b0fae0cdc475cccb8f76d66c7dabe33075
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 02:08:22 +0530

    Delete function

--- GadgetBadget/src/model/Product.java
+++ GadgetBadget/src/model/Product.java
@@ -157,4 +157,30 @@ public class Product {
 			 return output;
 			 }
 			
+			public String deleteProduct(String ProductID)
+			 {
+			 String output = "";
+			 try
+			 {
+			 Connection con = connect();
+			 if (con == null)
+			 {return "Error while connecting to the database for deleting."; }
+			 // create a prepared statement
+			 String query = "delete from products where ProductID=?";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 // binding values
+			 preparedStmt.setInt(1, Integer.parseInt(ProductID));
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Deleted successfully";
+			 }
+			 catch (Exception e)
+			 {
+			 output = "Error while deleting the product.";
+			 System.err.println(e.getMessage());
+			 }
+			 return output;
+			 }
+			
 }

b3829ad8043e2c301f4e139d4a24a6868fba2f04
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 02:12:18 +0530

    Get Method

--- GadgetBadget/src/com/ProductService.java
+++ GadgetBadget/src/com/ProductService.java
@@ -1,5 +1,23 @@
 package com;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Product;
+
+@Path("/Products")
 public class ProductService {
+	
+
+	Product productObj = new Product();
+	 @GET
+	 @Path("/")
+	 @Produces(MediaType.TEXT_HTML)
+	 public String readProducts()
+	  {
+	  return productObj.readProducts();
+	  }
 
 }

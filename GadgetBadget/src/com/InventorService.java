bf6e9eb0a7022e3c26007b3f3ac0b8ecf88e34da
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 01:39:37 +0530

    Post method

--- GadgetBadget/src/com/InventorService.java
+++ GadgetBadget/src/com/InventorService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -20,4 +23,17 @@ public class InventorService {
 	  }
 	
 	
+	 @POST
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String insertInventor(@FormParam("InventorName") String InventorName,
+	  @FormParam("InventorEmail") String InventorEmail,
+	  @FormParam("InventorContact") String InventorContact,
+	  @FormParam("InventorType") String InventorType)
+	 {
+	  String output = inventorObj.insertInventor(InventorName, InventorEmail,InventorContact, InventorType);
+	 return output;
+	 }
+	 
 }

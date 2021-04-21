91e26176651b510580dce407cbac95651278c85f
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 01:37:37 +0530

    Get method

--- GadgetBadget/src/com/InventorService.java
+++ GadgetBadget/src/com/InventorService.java
@@ -1,8 +1,23 @@
 package com;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import model.Inventor;
 
+@Path("/Inventors")
 public class InventorService {
+	
+	Inventor inventorObj = new Inventor();
+	 @GET
+	 @Path("/")
+	 @Produces(MediaType.TEXT_HTML)
+	 public String readInventors()
+	  {
+	  return inventorObj.readInventors();
+	  }
 	
 	
 }

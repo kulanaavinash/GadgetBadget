aac39d10fcbbc65d98ce2b1ba876e2ce373f7028
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 02:13:12 +0530

    Post method

--- GadgetBadget/src/com/ProductService.java
+++ GadgetBadget/src/com/ProductService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -19,5 +22,21 @@ public class ProductService {
 	  {
 	  return productObj.readProducts();
 	  }
+	 
+	 
+
+	 @POST
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String insertProduct(@FormParam("ProductName") String ProductName,
+	  @FormParam("ProductDate") String ProductDate,
+	  @FormParam("ProductDetails") String ProductDetails,
+	  @FormParam("ProductCategory") String ProductCategory)
+	 {
+	  String output =productObj.insertProduct(ProductName, ProductDate,ProductDetails, ProductCategory);
+	 return output;
+	 }
+	 
 
 }

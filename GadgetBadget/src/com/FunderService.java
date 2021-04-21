065deca8e4e0713aa393d9ecfb198fadb80e751e
IT19126784 <68727870+IT19126784@users.noreply.github.com>
2021-04-22 00:12:21 +0530

    Delete method

--- GadgetBadget/src/com/FunderService.java
+++ GadgetBadget/src/com/FunderService.java
@@ -1,6 +1,7 @@
 package com;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +9,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -57,5 +62,23 @@ public class FunderService {
 		  String output = funderObj.updateFunder(FunderID, FunderName, FunderEmail, FunderContact, FunderType);
 		  return output;
 		 }
+		
+		 @DELETE
+		 @Path("/")
+		 @Consumes(MediaType.APPLICATION_XML)
+		 @Produces(MediaType.TEXT_PLAIN)
+		 public String deleteFunder(String funderData)
+		 {
+		 //Convert the input string to an XML document
+		  Document doc = Jsoup.parse(funderData, "", Parser.xmlParser());
+
+		  //Read the value from the element <itemID>
+		  String FunderID = doc.select("FunderID").text();
+		  String output = funderObj.deleteFunder(FunderID);
+		 return output;
+		 }
+		 
+
+		
 		
 }

9ac8e79ecfb7dc761d8c4b2e546573b234d24235
Thejan Ganepola <ganepola66@gmail.coom>
2021-04-22 02:15:22 +0530

    Delete method

--- GadgetBadget/src/com/ProductService.java
+++ GadgetBadget/src/com/ProductService.java
@@ -1,6 +1,7 @@
 package com;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +9,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -58,6 +63,22 @@ public class ProductService {
 	  String ProductDetails = productObject.get("ProductDetails").getAsString();
 	  String ProductCategory = productObject.get("ProductCategory").getAsString();
 	  String output = productObj.updateProduct(ProductID, ProductName, ProductDate, ProductDetails, ProductCategory);
+	 return output;
+	 }
+	 
+
+	 @DELETE
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_XML)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String deleteProduct(String productData)
+	 {
+	 //Convert the input string to an XML document
+	  Document doc = Jsoup.parse(productData, "", Parser.xmlParser());
+
+	  //Read the value from the element <itemID>
+	  String ProductID = doc.select("ProductID").text();
+	  String output = productObj.deleteProduct(ProductID);
 	 return output;
 	 }
 	 

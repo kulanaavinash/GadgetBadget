2af3caeb9ecf07981170aaef7de57e9b325b344a
_Kaush_ <57662753+nipunakaushan@users.noreply.github.com>
2021-04-22 21:53:57 +0530

    Delete method

--- GadgetBadget/src/com/ApprovementService.java
+++ GadgetBadget/src/com/ApprovementService.java
@@ -1,6 +1,7 @@
 package com;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +9,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -64,4 +69,21 @@ public class ApprovementService {
 	 return output;
 	 }
 	 
+	 
+ 
+	 
+	 @DELETE
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_XML)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String deleteApprovement(String approvementData)
+	 {
+	 //Convert the input string to an XML document
+	  Document doc = Jsoup.parse(approvementData, "", Parser.xmlParser());
+
+	 //Read the value from the element <itemID>
+	  String ApproveID = doc.select("ApproveID").text();
+	  String output = approvementObj.deleteApprovement(ApproveID);
+	 return output;
+	 }
 }

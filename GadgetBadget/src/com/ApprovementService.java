fbd47b4d1b55b88bb0a5df99e4da3de87d4c6296
_Kaush_ <57662753+nipunakaushan@users.noreply.github.com>
2021-04-22 21:33:46 +0530

    DB Connection

--- GadgetBadget/src/com/ApprovementService.java
+++ GadgetBadget/src/com/ApprovementService.java
@@ -3,5 +3,22 @@ package com;
 public class ApprovementService {
 	
 	//DB connection
-	
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			 con= DriverManager.getConnection("jdbc:mysql://localhost:3306/paf_lab_03",
+			 "root", "");
+			 
+			 
+			// For testing
+			System.out.print("Successfully connected"); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
 }

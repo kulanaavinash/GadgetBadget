de10d80144fe628f9b7fdac882cb81fa064ad336
_Kaush_ <57662753+nipunakaushan@users.noreply.github.com>
2021-04-22 21:08:15 +0530

    Insert method

--- GadgetBadget/src/model/Approvement.java
+++ GadgetBadget/src/model/Approvement.java
@@ -2,6 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Approvement {
 
@@ -22,4 +24,39 @@ public class Approvement {
 
 		return con;
 	}
+	
+	
+	// insert method
+				public String insertCustomer(String name, String email, String type, String contact) {
+					Connection con = connect();
+					String output = "";
+					if (con == null) {
+						return "Error while connecting to the database";
+					}
+
+					// create a prepared statement
+					String query = " insert into customers (`CustomerID`,`CustomerName`,`CustomerEmail`,`CustomerType`,`CustomerContact`)"
+							+ " values (?, ?, ?, ?, ?)";
+					PreparedStatement preparedStmt;
+					try {
+						preparedStmt = con.prepareStatement(query);
+
+						preparedStmt.setInt(1, 0);
+						preparedStmt.setString(2, name);
+						preparedStmt.setString(3, email);
+						preparedStmt.setString(4, type);
+						preparedStmt.setString(5, contact);
+
+						preparedStmt.execute();
+						con.close();
+						output = "Inserted successfully";
+					} catch (SQLException e) {
+						output = "Error while inserting";
+						System.err.println(e.getMessage());
+					}
+					
+
+					return output;
+				}
+
 }

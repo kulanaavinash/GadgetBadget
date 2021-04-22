b2d5bddb1b206352e285eb45ddba7b951c1b4425
_Kaush_ <57662753+nipunakaushan@users.noreply.github.com>
2021-04-22 21:50:09 +0530

    Post method

--- GadgetBadget/src/com/ApprovementService.java
+++ GadgetBadget/src/com/ApprovementService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,5 +21,22 @@ public class ApprovementService {
 	  {
 	  return approvementObj.readApprovements();
 	  }
+	 
+	 
+	 
+	 
+	 
+	 @POST
+	 @Path("/")
+	 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String insertApprovement(@FormParam("ApproveStatus") String ApproveStatus,
+	  @FormParam("ApproveDate") String ApproveDate,
+	  @FormParam("ApproveDetails") String ApproveDetails,
+	  @FormParam("Endorser") String Endorser)
+	 {
+	  String output =approvementObj.insertApprovement(ApproveStatus, ApproveDate,ApproveDetails, Endorser);
+	 return output;
+	 }
 	 
 }
